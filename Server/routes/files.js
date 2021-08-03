204e8a0222f428b07eab963ece7c962273ea9e13
Shubham Gautam <72097846+Shub-Gautam@users.noreply.github.com>
2021-08-03 09:50:42 +0530

    download btn

--- Server/routes/files.js
+++ Server/routes/files.js
@@ -3,10 +3,13 @@ const multer = require("multer");
 const path = require("path");
 const File = require("../models/file");
 const { v4: uuid4 } = require("uuid");
+const dotenv = require("dotenv");
+const sendMail = require("../services/emailService");
+dotenv.config();
 
 let storage = multer.diskStorage({
-  destination: (request, file, cb) => cb(null, "uploads/"),
-  filename: (request, file, cb) => {
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => {
     const uniqueName = `${Date.now()}-${Math.round(
       Math.random() * 1e9
     )}${path.extname(file.originalname)}`;
@@ -19,29 +22,59 @@ let uplaod = multer({
   limit: { fileSize: 1000000 * 100 },
 }).single("myfile");
 
-router.post("/", (request, response) => {
-  if (!request.file) {
-    return res.json({ error: "All fields are required." });
-  }
-});
-//  Validate request
+router.post("/", (request, res) => {
+  // Store file
+  uplaod(request, res, async (err) => {
+    //  Validate request
+    if (!request.file) {
+      return res.json({ error: "All fields are required." });
+    }
 
-// Store file
-uplaod(request, res, async (err) => {
-  if (err) {
-    return res.status(500).send({ error: err.message });
-  }
-  // Store to Database
-  const file = new File({
-    filename: request.file.filename,
-    uuid: uuid4,
-    path: request.file.path,
-    size: request.file.size,
+    if (err) {
+      return res.status(500).send({ error: err.message });
+    }
+    // Store to Database
+    const file = new File({
+      filename: request.file.filename,
+      uuid: uuid4(),
+      path: request.file.path,
+      size: request.file.size,
+    });
+
+    const response = await file.save();
+    return res.json({
+      file: `${process.env.APP_BASE_URL}/files/${response.uuid}`,
+    });
   });
+});
+
+router.post("/send", async (req, res) => {
+  const { uuid, emailTo, emailFrom } = req.body;
+  //validate Request
+  if (!uuid || !emailTo || !emailFrom) {
+    return res.status(422).send({ error: "All fields are required" });
+  }
+
+  // Get data from database
+
+  const file = await File.findOne({ uuid: uuid });
+  if (file.sender) {
+    return res.status(422).send({ error: "Email already sent." });
+  }
+
+  file.sender = emailFrom;
+  file.receiver = emailTo;
 
   const response = await file.save();
-  return res.json({
-    file: `${process.env.APP_BASE_URL}/files/${response.uuid}`,
+
+  //send email
+  const sendmail = require("../services/emailService");
+  sendMail({
+    from: emailFrom,
+    to: emailTo,
+    subject: "File Sharing",
+    text: `${emailFrom} shared a file with you`,
+    html: require("../services/emailTemplate")({}),
   });
 });
 
--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -7,7 +8,16 @@ const PORT = 8080 || process.env.PORT;
 const connectDB = require("./config/db");
 connectDB();
 
+app.use(express.static("public"));
+
+//Template engine
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
 app.use("/api/files", require("./routes/files"));
+app.use("/files/download", require("./routes/download"));
+
+app.use("/files", require("./routes/show"));
 
 app.listen(PORT, () => {
   console.log(`Listning on port ${PORT}`);
