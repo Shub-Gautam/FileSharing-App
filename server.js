6a4b2e3cf79ae1434df293c2b09685c9a3259362
Shubham Gautam <72097846+Shub-Gautam@users.noreply.github.com>
2021-08-06 01:51:06 +0530

    / problem

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 app.use("/api/files", require("./routes/files"));
-app.use("/files", require("./routes/show"));
-app.use("/files/download", require("./routes/download"));
+app.use("files", require("./routes/show"));
+app.use("files/download", require("./routes/download"));
 
 app.listen(PORT, () => {
   console.log(`Listning on port ${PORT}`);
